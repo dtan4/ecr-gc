a5ab5ac3327280467c1843cde3544e0a709fdc5b
Daisuke Fujita <dtanshi45@gmail.com>
2016-10-18 15:10:40 +0900

    Delete all untagged images

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let AWS = require('aws-sdk');
+let ecr = new AWS.ECR();
+
+exports.handle = (event, context, callback) => {
+  let repository = event.repository;
+
+  let ecr = new AWS.ECR();
+  let promise = ecr.listImages({
+    repositoryName: repository,
+  }).promise();
+
+  promise.then(data => {
+    let imageIds = [];
+
+    data.imageIds.forEach(image => {
+      if (!image.imageTag) {
+        console.log('image ' + image.imageDigest + ' will be deleted.');
+
+        imageIds.push({
+          imageDigest: image.imageDigest,
+        });
+      }
+    });
+
+    return ecr.batchDeleteImage({
+      imageIds: imageIds,
+      repositoryName: repository,
+    }).promise();
+  }).then(data => {
+    callback(null, data);
+  }).catch(err => {
+    callback(err);
+  });
+};
