f0db22db7354486f5d8bb5fa755f290e8ed14db3
Daisuke Fujita <dtanshi45@gmail.com>
2018-01-23 10:11:04 +0900

    Dockerize

---
+++ entrypoint.js
@@ -0,0 +1,12 @@
+let index = require('./index.js');
+
+index.handle({
+  strategy: process.env.GC_STRATEGY,
+}, null, (error, result) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  console.log(result);
+});
