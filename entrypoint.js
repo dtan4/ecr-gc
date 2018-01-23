a48a765d16857e674bf5a435783f442f3997300e
Daisuke Fujita <dtanshi45@gmail.com>
2018-01-23 11:03:03 +0900

    Use REPOSITORIES and check the variables

--- entrypoint.js
+++ entrypoint.js
@@ -2,7 +2,16 @@
 
 let index = require('./index.js');
 
+if (process.env.REPOSITORIES === undefined) {
+  throw new Error('REPOSITORIES must be set');
+}
+
+if (process.env.GC_STRATEGY === undefined) {
+  throw new Error('GC_STRATEGY must be set');
+}
+
 index.handle({
+  repositories: process.env.REPOSITORIES.split(','),
   strategy: process.env.GC_STRATEGY,
 }, null, (error, result) => {
   if (error) {
