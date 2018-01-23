ae8d985199228e92916ca3258ad59f97b298d6c3
Daisuke Fujita <dtanshi45@gmail.com>
2018-01-23 11:29:22 +0900

    Accept empty REPOSITORIES

--- entrypoint.js
+++ entrypoint.js
@@ -2,16 +2,12 @@
 
 let index = require('./index.js');
 
-if (process.env.REPOSITORIES === undefined) {
-  throw new Error('REPOSITORIES must be set');
-}
-
 if (process.env.GC_STRATEGY === undefined) {
   throw new Error('GC_STRATEGY must be set');
 }
 
 index.handle({
-  repositories: process.env.REPOSITORIES.split(','),
+  repositories: process.env.REPOSITORIES === undefined ? '' : process.env.REPOSITORIES.split(','),
   strategy: process.env.GC_STRATEGY,
 }, null, (error, result) => {
   if (error) {
